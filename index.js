cf5ec70b76c841f8781fe465b3c2adbbb72f9756
Muhammad Abbas <mohammed.petiwala52@gmail.com>
2018-03-07 23:23:24 +0500

    first express app

---
+++ index.js
@@ -0,0 +1,33 @@
+let express = require('express');
+
+let app = express();
+
+app.route('/')
+  .get(function(req, res) {
+    if (req.path == '/') {
+      res.sendFile(process.cwd() + '/views/index.html');
+    } else {
+      res.send({
+        name: "hello world"
+      });
+    }
+  });
+
+// Respond not found to all the wrong routes
+app.use(function(req, res, next) {
+  res.status(404);
+  res.type('txt').send('Not found');
+});
+
+// Error Middleware
+app.use(function(err, req, res, next) {
+  if (err) {
+    res.status(err.status || 500)
+      .type('txt')
+      .send(err.message || 'SERVER ERROR');
+  }
+});
+
+app.listen(process.env.PORT || 5000, function() {
+  console.log('Node.js listening ...');
+});
